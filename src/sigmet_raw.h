a21633436cc62f53355285e66287dfde25d03d1e
Gordon Carrie <dev0@trekix.net>
2010-02-15 21:38:20 +0000

    Made callbacks local to daemon program. Made name of input fifo global

--- src/sigmet_raw.h
+++ src/sigmet_raw.h
@@ -1,36 +1,19 @@
 /*
    -	sigmet_raw.h --
-   -		This header file declares structures and functions
-   -		for the sigmet_raw application.
+   -		Declarations for the sigmet_raw daemon and client.
    -
    .	Copyright (c) 2009 Gordon D. Carrie.
    .	All rights reserved.
    .
    .	Please send feedback to user0@tkgeomap.org
    .
-   .	$Revision: 1.2 $ $Date: 2010/01/13 23:19:28 $
+   .	$Revision: 1.3 $ $Date: 2010/01/14 15:27:03 $
  */
 
 #ifndef SIGMET_RAW_H_
 #define SIGMET_RAW_H_
 
-typedef int (SigmetRaw_Callback)(int , char **);
-SigmetRaw_Callback SigmetRaw_Types_Cb;
-SigmetRaw_Callback SigmetRaw_Good_Cb;
-SigmetRaw_Callback SigmetRaw_Read_Cb;
-SigmetRaw_Callback SigmetRaw_Volume_Headers_Cb;
-SigmetRaw_Callback SigmetRaw_Ray_Headers_Cb;
-SigmetRaw_Callback SigmetRaw_Data_Cb;
-SigmetRaw_Callback SigmetRaw_Bin_Outline_Cb;
-SigmetRaw_Callback SigmetRaw_Bintvls_Cb;
-
-int SigmetRaw_AddFile(FILE *);
-void SigmetRaw_RmFile(FILE *);
-FILE ** SigmetRaw_GetFiles(long *);
-void SigmetRaw_UseDeg(void);
-void SigmetRaw_UseRad(void);
-int SigmetRaw_CmdI(const char *);
-SigmetRaw_Callback * SigmetRaw_Cmd(const char *);
-void SigmetRaw_CleanUp(void);
+/* Name of fifo from which daemon will read commands */
+#define SIGMET_RAWD_IN "sigmet.in"
 
 #endif
