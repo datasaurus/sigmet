f8d00c9ad7dd2d18e293dc7938579911ea4cea53
Gordon Carrie <dev0@trekix.net>
2010-08-18 19:42:04 +0000

    Moved projection management to a separate file

--- src/sigmet_raw.h
+++ src/sigmet_raw.h
@@ -7,16 +7,22 @@
    .
    .	Please send feedback to user0@tkgeomap.org
    .
-   .	$Revision: 1.5 $ $Date: 2010/07/02 22:15:47 $
+   .	$Revision: 1.6 $ $Date: 2010/07/29 20:22:49 $
  */
 
 #ifndef SIGMET_RAW_H_
 #define SIGMET_RAW_H_
+
+#include <proj_api.h>
 
 /* Daemon socket */
 #define SIGMET_RAWD_IN "sigmet.in"
 
 /* Maximum number of arguments */
 #define SIGMET_RAWD_ARGCX	512
+
+int SigmetRaw_ProjInit(void);
+int SigmetRaw_SetProj(int, char **);
+projPJ SigmetRaw_GetProj(void);
 
 #endif
