68d30a83b52b67af5e8f96a940b1274f28487f3f
Gordon Carrie <dev0@trekix.net>
2010-11-10 16:25:32 +0000

    Edited source documentation.

--- src/sigmet_data.c
+++ src/sigmet_data.c
@@ -1,13 +1,14 @@
 /*
    -	sigDataType.c --
-   -		Define functions that manipulate Sigmet data.
+   -		This file defines functions that manipulate Sigmet data.
+   -		See sigmet (3).
    -
-   .	Copyright (c) 2004 Gordon D. Carrie
+   .	Copyright (c) 2010 Gordon D. Carrie
    .	All rights reserved
    .
    .	Please send feedback to dev0@trekix.net
    .
-   .	$Revision: 1.14 $ $Date: 2010/10/20 16:23:28 $
+   .	$Revision: 1.15 $ $Date: 2010/10/22 22:28:19 $
    .
    .	Reference: IRIS Programmers Manual
  */
@@ -17,11 +18,17 @@
 #include "err_msg.h"
 #include "sigmet.h"
 
-/* 2^16 and 2^32 */
+/*
+   Abbreviations for 2^16 and 2^32
+ */
+
 #define TWO_16 ((double)((unsigned)0xFFFF) + 1.0)
 #define TWO_32 ((double)((unsigned)0xFFFFFFFF) + 1.0)
 
-/* Short names for Sigmet data types.  Index with enum Sigmet_DataType. */
+/*
+   Short names for Sigmet data types.  Index with enum Sigmet_DataType.
+ */
+
 static char *abbrv[SIGMET_NTYPES] = {
     "DB_XHDR",	"DB_DBT",	"DB_DBZ",	"DB_VEL",	"DB_WIDTH",
     "DB_ZDR",	"DB_DBZC",	"DB_DBT2",	"DB_DBZ2",	"DB_VEL2",
@@ -31,7 +38,10 @@ static char *abbrv[SIGMET_NTYPES] = {
     "DB_LDRH2",	"DB_LDRV",	"DB_LDRV2", 	"DB_ERROR"
 };
 
-/* Descriptors for Sigmet data types. Index with enum Sigmet_DataType. */
+/*
+   Descriptors for Sigmet data types. Index with enum Sigmet_DataType.
+ */
+
 static char *descr[SIGMET_NTYPES] = {
     "Extended header",
     "Uncorrected reflectivity (1 byte)",
@@ -84,13 +94,11 @@ unsigned long Sigmet_RadBin2(double a)
     return a * TWO_16 / (2 * PI);
 }
 
-/* Fetch the short name of a Sigmet data type */
 char * Sigmet_DataType_Abbrv(enum Sigmet_DataType y)
 {
     return (y < SIGMET_NTYPES) ? abbrv[y] : NULL;
 }
 
-/* Fetch the descriptor of a Sigmet data type */
 char * Sigmet_DataType_Descr(enum Sigmet_DataType y)
 {
     return (y < SIGMET_NTYPES) ? descr[y] : NULL;
